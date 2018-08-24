d0773ff5d000a6a301269ee082fb0307b1e3e4c5
e <1721359644@qq.com>
2018-08-24 10:29:46 +0800

    mybatis-generator

---
+++ src/main/java/bean/Example.java
@@ -0,0 +1,43 @@
+package bean;
+
+public class Example {
+    private Integer id;
+
+    private String what;
+
+    private String is;
+
+    private String love;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getWhat() {
+        return what;
+    }
+
+    public void setWhat(String what) {
+        this.what = what == null ? null : what.trim();
+    }
+
+    public String getIs() {
+        return is;
+    }
+
+    public void setIs(String is) {
+        this.is = is == null ? null : is.trim();
+    }
+
+    public String getLove() {
+        return love;
+    }
+
+    public void setLove(String love) {
+        this.love = love == null ? null : love.trim();
+    }
+}
