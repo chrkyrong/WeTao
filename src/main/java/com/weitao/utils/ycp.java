73e5ed2edab9df9644215020d3a9de45a1a8fecb
ycp <1047700877@qq.com>
2018-09-04 08:15:48 +0800

    test

---
+++ src/main/java/com/weitao/utils/ycp.java
@@ -0,0 +1,10 @@
+package com.weitao.utils;
+
+/**
+ * Created by ycp on 2018/9/4.
+ */
+public class ycp {
+    public  static  void  main(String args[]){
+        System.out.println("sss");
+    }
+}
