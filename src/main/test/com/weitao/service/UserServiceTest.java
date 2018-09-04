af85522979c8b3c669862414fd8fd06d4aaedd1a
chrkyrong <1721359644@qq.com>
2018-09-04 16:13:33 +0800

    test

--- src/main/test/com/weitao/service/UserServiceTest.java
+++ src/main/test/com/weitao/service/UserServiceTest.java
@@ -1,14 +1,11 @@
 package com.weitao.service;
 
 import com.weitao.bean.User;
-import com.weitao.dao.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import static org.junit.Assert.*;
 
 /**
  * Created by lzr on 2018/9/4.
