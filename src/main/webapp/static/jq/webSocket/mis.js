686d75ff8ad42c5d5eee2b399490806f1da53164
huzebin <2215881237@qq.com>
2018-10-07 07:40:08 +0800

    修改购物车以及收藏夹中的价格单位符号

--- src/main/webapp/static/jq/webSocket/mis.js
+++ src/main/webapp/static/jq/webSocket/mis.js
@@ -20,7 +20,7 @@ function select() {
             var s = "";
             $.each(r.data,function (i,v) {
                 s+='<tr class="cart_item"><td class="product-thumbnail" width="300"><a href="shop_single.html?iId='+v.itemsId+'">';
-                s+='<img width="80" height="80" src="static/images/'+v.iPhotos+'" class="attachment-shop_thumbnail size-shop_thumbnail wp-post-image"></a>'+'</td><td data-title="Product" width="500"><div class="caption"><a class="product-name" href="shop_single.html?iId='+v.itemsId+'">'+v.iName+'</a></div></td><td  class="product-price" data-title="Price"><span class="product-price total-price"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">£</span>'+v.iPrice+'</span></span></td>';
+                s+='<img width="80" height="80" src="static/images/'+v.iPhotos+'" class="attachment-shop_thumbnail size-shop_thumbnail wp-post-image"></a>'+'</td><td data-title="Product" width="500"><div class="caption"><a class="product-name" href="shop_single.html?iId='+v.itemsId+'">'+v.iName+'</a></div></td><td  class="product-price" data-title="Price"><span class="product-price total-price"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">￥</span>'+v.iPrice+'</span></span></td>';
                 s+='<td class="product-remove"><a onclick="del('+v.cId+',this)" class="btn btn-remove" title="Remove this item"><i class="fa fa-trash fa-lg"></i></a></td>';
                 s+='</tr>';
             })
