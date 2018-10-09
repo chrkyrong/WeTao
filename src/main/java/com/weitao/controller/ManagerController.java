4eaad1d5c70ad81a32db17dbc50dca9291a1755d
Cc Technology <1377228342@qq.com>
2018-10-09 10:25:21 +0800

    '管理员controller'

---
+++ src/main/java/com/weitao/controller/ManagerController.java
@@ -0,0 +1,18 @@
+package com.weitao.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author:Cc
+ * @Date:2018/10/9
+ * @program: weitao
+ * @description: ${管理员的controller}
+ * @create: 2018-10-09 10:14
+ */
+
+@RestController
+@RequestMapping("/manager")
+public class ManagerController {
+
+}
